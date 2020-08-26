9b05a9f5c8be3d3bc95d3f9a9fc966fed76477cc
wbh1328551759 <1328551759@qq.com>
2020-08-26 22:59:03 +0800

    添加 loading 状态

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,10 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <g-icon class="icon" v-if="icon" :name="icon"/>
+    <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
+    <g-icon class="loading" name="loading"></g-icon>
+
     <div class="content">
-      <slot/>
+      <slot></slot>
     </div>
   </button>
 </template>
@@ -23,6 +25,10 @@
 </script>
 
 <style lang="scss">
+  @keyframes spin {
+    0%{ transform: rotate(0deg); }
+    100%{ transform: rotate(360deg); }
+  }
   .g-button {
     font-size: var(--font-size);
     height: var(--button-height);
@@ -66,6 +72,9 @@
         margin-right: 0;
         margin-left: .1em;
       }
+    }
+    .loading{
+      animation: spin 2s infinite linear;
     }
   }
 
