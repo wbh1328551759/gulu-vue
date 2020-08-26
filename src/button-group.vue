4e579d49ad13948cee56a03bab810fcb5bc10d69
wbh1328551759 <1328551759@qq.com>
2020-08-26 23:30:21 +0800

    创建 button-group

---
+++ src/button-group.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="g-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  .g-button-group{
+    display: inline-flex;
+    vertical-align: middle;
+    .g-button{
+      margin-left: -1px;
+      border-radius: 0;
+      &:first-child{
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child{
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      &:hover{
+        position: relative;
+        z-index: 1;
+      }
+    }
+
+  }
+</style>
