b14353b959542cf5a1dae96f60c864c912cbef09
wbh1328551759 <1328551759@qq.com>
2020-08-28 23:56:02 +0800

    使用 npm run dev-test 来自动化测试

--- src/app.js
+++ src/app.js
@@ -19,83 +19,92 @@ new Vue({
 //单元测试
 import chai from 'chai'
 import spies from 'chai-spies'
+
 chai.use(spies)
 
-const expect = chai.expect
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-    }
-  })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-setting')
-  vm.$el.remove()
-  vm.$destroy()
-}
+try {
+  const expect = chai.expect
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+      }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-setting')
+    vm.$el.remove()
+    vm.$destroy()
+  }
 
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      loading: true
-    }
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+        loading: true
+      }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+      }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+        iconPosition: 'right'
+      }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+  }
+  {
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+      propsData: {
+        icon: 'setting',
+      }
+    })
+    vm.$mount()
+    let spy = chai.spy(function () {})
+    vm.$on('click', spy)
+    let button = vm.$el
+    button.click()
+    expect(spy).to.have.been.called()
+  }
+}catch (error) {
+  window.errors = [error]
+}finally {
+  window.errors && window.errors.forEach((error) => {
+    console.error(error.message)
   })
-  vm.$mount()
-  let useElement = vm.$el.querySelector('use')
-  let href = useElement.getAttribute('xlink:href')
-  expect(href).to.eq('#i-loading')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('1')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const div = document.createElement('div')
-  document.body.appendChild(div)
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-      iconPosition: 'right'
-    }
-  })
-  vm.$mount(div)
-  let svg = vm.$el.querySelector('svg')
-  let {order} = window.getComputedStyle(svg)
-  expect(order).to.eq('2')
-  vm.$el.remove()
-  vm.$destroy()
-}
-{
-  const Constructor = Vue.extend(Button)
-  const vm = new Constructor({
-    propsData: {
-      icon: 'setting',
-    }
-  })
-  vm.$mount()
-  let spy = chai.spy(function(){})
-  vm.$on('click', spy)
-  let button = vm.$el
-  button.click()
-  expect(spy).to.have.been.called()
 }
--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,11 @@
 </template>
 
 <script>
+  import Icon from './icon.vue'
   export default {
+    components:{
+      'g-button': Icon
+    },
     props: {
       icon: {},
       loading: {
