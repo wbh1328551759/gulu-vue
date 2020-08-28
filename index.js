6424ef2f2cf8998e9090f27132e271163a7ed8e7
wbh1328551759 <1328551759@qq.com>
2020-08-29 03:21:51 +0800

    更新

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import Button from './src/button'
-import ButtonGroup from './src/button-group'
-import Icon from './src/icon'
+import Button from './src/button.vue'
+import ButtonGroup from './src/button-group.vue'
+import Icon from './src/icon.vue'
 
 export {Button, ButtonGroup, Icon}
