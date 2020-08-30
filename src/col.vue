9198e00434e5a37c58e4d77a8e075d0868a7feb9
wbh1328551759 <1328551759@qq.com>
2020-08-30 22:38:50 +0800

    重构 cow 和 row

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-  :style="{paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px'}">
-    <div style="border: 1px solid green; height: 100px">
-      <slot></slot>
-    </div>
+  <div class="col" :class="colClass" :style="colStyle">
+    <slot></slot>
   </div>
 </template>
 
@@ -16,32 +13,46 @@
       },
       offset: {
         type: [Number, String]
-      },
-
+      }
     },
-    data(){
+    data() {
       return {
-        gutter: 0
+        gutter: 0,
+      }
+    },
+    computed: {
+      colClass() {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle() {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px'
+        }
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-.col{
-  width: 50%;
+  .col {
+    width: 50%;
 
-  $class-prefix: col-;
-  @for $n from 1 through 24{
-    &.#{$class-prefix}#{$n}{
-      width: ($n / 24) * 100%
+    $class-prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        width: ($n / 24) * 100%
+      }
+    }
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%
+      }
     }
   }
-  $class-prefix: offset-;
-  @for $n from 1 through 24{
-    &.#{$class-prefix}#{$n}{
-      margin-left: ($n / 24) * 100%
-    }
-  }
-}
 </style>
