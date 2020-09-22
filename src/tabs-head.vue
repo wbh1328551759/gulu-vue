e4bb2a1ea7aad3dfe1dde8e055063750bbb5a97a
wbh1328551759 <1328551759@qq.com>
2020-09-22 16:02:04 +0800

    添加 tabs 相关组件

---
+++ src/tabs-head.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tabs-head">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuluTabsHead",
+  props: {}
+};
+</script>
+
+<style>
+.tabs-head {
+}
+</style>
---
+++ src/tabs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tabs">
+    <slot></slot>
+    <slot name="action"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuluTabs",
+  props: {
+    selected: {
+      type: String,
+      required: true
+    },
+    direction: {
+      type: String,
+      default: "horizontal",
+      validator(value) {
+        return ["horizontal", "vertical"].indexOf >= 0;
+      }
+    }
+  },
+  created() {}
+};
+</script>
+
+<style>
+.tabs {
+}
+</style>
