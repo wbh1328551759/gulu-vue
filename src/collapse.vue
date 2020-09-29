c61c7e1290b6134d8425de38a361d983278ba17d
wbh1328551759 <1328551759@qq.com>
2020-09-29 23:56:22 +0800

    基本完成collapse组件

---
+++ src/collapse.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="collapse">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+export default {
+  name: "GuluCollapse",
+  data() {
+    return {
+      eventBus: new Vue()
+    };
+  },
+  provide() {
+    return {
+      eventBus: this.eventBus
+    };
+  },
+  props: {
+    single: {
+      type: Boolean,
+      default: false
+    },
+    selected: {
+      type: String
+    }
+  },
+  mounted() {
+    this.eventBus.$emit("update:selected", this.selected);
+  }
+};
+</script>  
+
+<style lang="scss" scoped>
+$grey: #ddd;
+$border-radius: 4px;
+.collapse {
+  border: 1px solid $grey;
+  border-radius: $border-radius;
+}
+</style>
